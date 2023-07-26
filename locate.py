33bc558af94967c548a8b3428122329dce59a3da
CiprianoJr <claudio.cipriano@outlook.com>
2023-07-26 02:18:04 -0300

    first commit

--- locate.py
+++
@@ -1,5 +0,0 @@
-import pyautogui
-
-# Caminho da imagem salva no computador
-imagem_salva_path = 'caminho/para/sua/imagem_salva.png'
-imagem_salva = cv2.imread(imagem_salva_path)
---
+++ teste.py
@@ -0,0 +1,24 @@
+import pyautogui
+import cv2
+import numpy as np
+
+pyautogui.sleep(3)
+
+pyautogui.mouseDown(268, 769, button='left')
+pyautogui.mouseUp(963, 660, button='left')
+pyautogui.mouseDown(287, 710,button='left')
+pyautogui.mouseUp(963, 660, button='left')
+pyautogui.mouseDown(302, 653,button='left')
+pyautogui.mouseUp(963, 660, button='left')
+pyautogui.mouseDown(314, 606,button='left')
+pyautogui.mouseUp(963, 660, button='left')
+pyautogui.mouseDown(308, 740,button='left')
+pyautogui.mouseUp(963, 660, button='left')
+pyautogui.mouseDown(357, 655,button='left')
+pyautogui.mouseUp(963, 660, button='left')
+pyautogui.mouseDown(363, 653,button='left')
+pyautogui.mouseUp(963, 660, button='left')
+pyautogui.mouseDown(382, 676,button='left')
+pyautogui.mouseUp(963, 660, button='left')
+pyautogui.mouseDown(415, 651,button='left')
+pyautogui.mouseUp(963, 660, button='left')
